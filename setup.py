5722bde0bc81eec878c51934ed2c8c6101f95cdb
Nat1an <nathan@nat1an.xyz>
2023-04-08 21:27:43 +0800

    🔧 chore(requirements.txt): add inquirer and pyyaml packages 🔧 chore(setup.py): change default_configs to configs directory 🚀 perf(setup.py): add success, warning and info functions to improve readability 🔧 chore(setup.py): write port to lavalink.json and application.yml files

--- setup.py
+++ setup.py
@@ -1,10 +1,26 @@
+import json
 import shutil
-from os import getenv, listdir, path, mkdir, rename
+from os import getenv, listdir, path, mkdir, rename, remove
 from os.path import isdir
+from typing import Any
 
 import jdk
 import requests
+import yaml
 from git import Repo, NoSuchPathError
+from inquirer import text, confirm
+
+
+def info(obj: Any):
+    print(f"[\033[36mi\033[0m] {str(obj)}")
+
+
+def warning(obj: Any):
+    print(f"[\033[33m!\033[0m] {str(obj)}\n")
+
+
+def success(obj: Any):
+    print(f"[\033[32m+\033[0m] {str(obj)}\n")
 
 
 def main():
@@ -12,13 +28,17 @@ def main():
 
     update_lava(repo)
 
+    fill_secrets()
+
+    set_ports()
+
     setup_lava()
 
     get_java()
 
     get_lavalink()
 
-    print("Setup complete!, Please run startup.sh to start Lava!")
+    success("Setup complete!, Please run startup.sh to start Lava!")
 
 
 def clone_lava() -> Repo:
@@ -30,39 +50,110 @@ def clone_lava() -> Repo:
         repo = Repo("./Lava")
 
     except NoSuchPathError:
-        print("Cloning Lava...")
+        info("Cloning Lava...")
 
         repo = Repo.clone_from(
             getenv("git_repo", "https://github.com/Nat1anWasTaken/Lava.git"), "./lava", branch="master"
         )
 
-    print("Lava cloned successfully!")
+    success("Lava cloned successfully!")
 
     return repo
 
 
 def update_lava(repo: Repo):
-    print("Updating Lava...")
+    info("Updating Lava...")
 
     remote = repo.remote("origin")
 
     remote.pull()
 
-    print("Lava updated successfully!")
+    success("Lava updated successfully!")
+
+
+def fill_secrets():
+    if path.isfile(".env"):
+        warning(".env file already exists, skipping...")
+
+        return
+
+    env_file = open(".env", "w", encoding="utf-8")
+
+    info(".env file doesn't exists, creating one...")
+
+    try:
+        token = text("TOKEN")
+
+        env_file.write(f"TOKEN={token}\n")
+
+        if confirm("Do you want to enable Spotify support?"):
+            spotify_client_id = text("Spotify Client ID")
+            env_file.write(f"SPOTIFY_CLIENT_ID={spotify_client_id}\n")
+
+            spotify_client_secret = text("Spotify Client Secret")
+            env_file.write(f"SPOTIFY_CLIENT_SECRET={spotify_client_secret}\n")
+
+        env_file.close()
+
+    except KeyboardInterrupt:
+        env_file.close()
+
+        remove(".env")
+
+        raise KeyboardInterrupt
+
+    success(".env file created successfully!")
+
+    return
+
+
+def set_ports():
+    if path.isfile("lava/configs/lavalink.json") and path.isfile("lavalink/application.yml"):
+        warning("lavalink.json and application.yml file already exists, skipping...")
+
+        return
+
+    port = text(
+        "Please enter the port for lavalink",
+        default="2333",
+        validate=lambda _, x: x.isdigit()
+    )
+
+    with open("configs/lavalink.json", "r+", encoding="utf-8") as f:
+        data = json.load(f)
+
+        data["nodes"][0]["port"] = int(port)
+
+        f.seek(0)
+
+        f.write(json.dumps(data, indent=4))
+
+    with open("configs/application.yml", "r+", encoding="utf-8") as f:
+        data = yaml.load(f, Loader=yaml.FullLoader)
+
+        data["server"]["port"] = int(port)
+
+        f.seek(0)
+
+        f.write(yaml.dump(data, indent=4))
+
+    success("Wrote port to lavalink.json and application.yml successfully!")
+
+    return
 
 
 def setup_lava():
-    print("Setting up Lava...")
+    info("Setting up Lava...")
 
-    shutil.copyfile("default_configs/lavalink.json", "lava/configs/lavalink.json")
+    shutil.copyfile("configs/lavalink.json", "lava/configs/lavalink.json")
 
-    shutil.copyfile("default_configs/icons.json", "lava/configs/icons.json")
+    shutil.copyfile("configs/icons.json", "lava/configs/icons.json")
 
-    print("Lava setup successfully!")
+    success("Lava setup successfully!")
 
 
 def get_java():
-    print("Installing JDK...")
+    info("Installing JDK...")
 
     if not isdir("./java"):
         try:
@@ -75,11 +166,11 @@ def get_java():
             rename(f"./java/{directory}", f"./java/jdk")
             break
 
-    print("JDK installed successfully!")
+    success("JDK installed successfully!")
 
 
 def get_lavalink():
-    print("Installing Lavalink...")
+    info("Installing Lavalink...")
 
     if path.isfile("./lavalink/Lavalink.jar"):
         return
@@ -94,9 +185,9 @@ def get_lavalink():
     with open("./lavalink/Lavalink.jar", 'wb') as f:
         f.write(jar.content)
 
-    shutil.copyfile("default_configs/application.yml", "lavalink/application.yml")
+    shutil.copyfile("configs/application.yml", "lavalink/application.yml")
 
-    print("Lavalink installed successfully!")
+    success("Lavalink installed successfully!")
 
 
 if __name__ == "__main__":
