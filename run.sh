fa59e05fa645dc0916ed086fbb7279b3b01305ea
Nat1an <nathan@nat1an.xyz>
2023-04-08 22:05:18 +0800

    🔒 chore(run.sh): export environment variables from .env file before running lava and lavalink

--- run.sh
+++ run.sh
@@ -6,11 +6,13 @@ python3 setup.py
 python3 -m pip install -r lava/requirements.txt
 
 (
-  source .env
+  export TOKEN=$(sed -n 's/^DTOKEN=//p' .env)
+  export SPOTIFY_CLIENT_ID=$(sed -n 's/^SPOTIFY_CLIENT_ID=//p' .env)
+  export SPOTIFY_CLIENT_SECRET=$(sed -n 's/^SPOTIFY_CLIENT_SECRET=//p' .env)
+
   cd lava
   python3 main.py
-) &
-(
+) & (
   cd lavalink
   ../java/jdk/bin/java -jar Lavalink.jar
 )
