592b54ae75c41e5d1fb8b5faabc0a1a54660e8ea
Nat1an <nathan@nat1an.xyz>
2023-04-08 21:54:34 +0800

    🐛 fix(run.sh): use python3 -m pip instead of pip to install requirements ✨ feat(run.sh): run Lavalink in the background

--- run.sh
+++ run.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
-pip install -r requirements.txt
+python3 -m pip install -r requirements.txt
 
 python3 setup.py
 
-pip install -r lava/requirements.txt
+python3 -m pip install -r lava/requirements.txt
 
 source .env
 
 (
   cd lava
   python3 main.py
-) & (
+) &
+(
   cd lavalink
   ../java/jdk/bin/java -jar Lavalink.jar
 )
