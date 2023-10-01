43c04574b3818113b411086d5eb72898d2866b3c
Mantou <51238168+Mantou-9487@users.noreply.github.com>
2023-10-01 20:18:13 +0800

    Fix(run.py): script execution and path issues

--- run.py
+++ run.py
@@ -4,11 +4,8 @@ import sys
 import subprocess
 
 if sys.platform == "win32":
-    file = "run.ps1"
     os.system("powershell Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy RemoteSigned")
-    p = subprocess.Popen(["powershell.exe", 
-              f".\{file}"], 
-              stdout=sys.stdout)
+    p = subprocess.Popen(["powershell.exe", r".\\run.ps1"], stdout=sys.stdout)
     p.communicate()
 else:
     os.system("./run.sh")
