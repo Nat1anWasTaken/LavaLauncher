fdb8a8aa629147b63d762b95182b88f9200cbc34
Mantou <51238168+Mantou-9487@users.noreply.github.com>
2023-10-01 20:05:25 +0800

    Fix(run.py): PowerShell script execution issue

--- run.py
+++ run.py
@@ -1,7 +1,14 @@
 import os
 import sys
 
+import subprocess
+
 if sys.platform == "win32":
-    os.system("powershell run.ps1")
+    file = "run.ps1"
+    os.system("powershell Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy RemoteSigned")
+    p = subprocess.Popen(["powershell.exe", 
+              f".\{file}"], 
+              stdout=sys.stdout)
+    p.communicate()
 else:
     os.system("./run.sh")
